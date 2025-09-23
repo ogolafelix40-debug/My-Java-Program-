1582c5e331719481e192f2a1e999d95da21dc7ec
ogolafelix40-debug <ogolafelix40@gmail.com>
2025-09-23 14:04:27 +0300

    Add files via upload

---
+++ Felix Assignment.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.utl.DateFormat;
+import java.util.LocalDate;
+public class LibraryFine{
+    publi static void main(String[] args){
+    Scanner scanner = new scanner(System.in);
+    double LibraryFine, 
+    double bookID, returnDate, dueDate;
+    //prompt user user to enter date
+    system.out.ln("Enter book ID:");
+    bookID = scanner.nextDouble();
+    system.out.ln("Enter return date (DD-MM-YYYY):");
+    returnDate = scanner.nextDouble();
+    system.out.ln("Enter due date (DD-MM-YYYY):");
+    dueDate = scanner next.Double();
+
+    if(days<=7){
+        LibraryFine = 20* each day;
+    }
+    else if(days<=14){
+        LibraryFine = 50* each day;
+    }
+    else(days>=15){
+        LibraryFine = 100* each day;
+        
+       double overdueDate = returnDate _ duedate;
+       double fineRate = overdueDate/LibabryFine
+}
+  system.out.ln("bookID:");
+  system.out.ln("dueDate:");
+  system.out.ln("returnDate:");
+  system.out.ln("overdueDate:");
+  system.out.ln("LibraryFine:");
+  system.out.ln("fineRate:");
+
+  scanner.close();
+} 
