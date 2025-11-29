6f8c3b9653f94bf7659f1af089ce1dd50697a1ea
ogolafelix40-debug <ogolafelix40@gmail.com>
2025-11-29 17:51:56 +0300

    Add files via upload

---
+++ Coffee.java
@@ -0,0 +1,29 @@
+class CoffeeOrder{
+    String size;
+    double price;
+
+    //constructor
+    CoffeeOrder(String size, double price){
+        this.size = size;
+        this.price = price;
+    }
+
+    void display(){
+        System.out.println(" Size:" + size);
+        System.out.println("Price:" + price);
+    }
+}
+
+public class Coffee{
+    public static void main(String[] args) {
+        CoffeeOrder order1 = new CoffeeOrder("small", 100);
+
+        CoffeeOrder order2 = new CoffeeOrder("large", 150);
+
+        System.out.println("Display Details");
+        order1.display();
+        order2.display();
+
+        
+    }
+}
